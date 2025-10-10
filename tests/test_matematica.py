9cd55df8ce64d7337d51d59c0e27e8f911cc61a9
Charles Pereira <charles.p@edu.sc.senai.br>
2025-10-10 17:29:41 +0000

    feature: test_texto

--- tests/test_matematica.py
+++ tests/test_matematica.py
@@ -1,6 +1,5 @@
 from app.matematica import Matematica
 
 def test_somar():
-  assert Matematica.somar(1, 1) == 2
-  assert Matematica.somar(-1, 1) == 0
-  assert Matematica.somar(0, 0) == 0
+  assert Matematica.somar(1, 2) == 2
+
